a4372fceb638c8944b8e0957f8fb3fc40bcf1ecb
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2020-12-29 10:24:47 +0300

    Update trigger.sql

--- sql-kodlar/trigger.sql
+++ sql-kodlar/trigger.sql
@@ -1,1 +1,18 @@
+--Trigger işlemleri
 
+ CREATE OR REPLACE FUNCTION befor_insert_personel()
+   RETURNS trigger AS
+ $$
+ BEGIN
+ NEW.ad= UPPER(NEW.ad);
+ NEW.soyad= UPPER(NEW.soyad);
+ RETURN NEW;
+ END;
+ $$
+ LANGUAGE 'plpgsql';
+
+ CREATE TRIGGER trg_Ad_soyad
+   BEFORE INSERT
+ON personel
+   FOR EACH ROW
+   EXECUTE PROCEDURE befor_insert_personel();
