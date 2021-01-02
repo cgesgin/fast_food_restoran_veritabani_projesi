4c3799ff319a1d4189e928766b314da9156488c8
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-01-02 18:11:45 +0300

    Update cascade.sql

--- sql-kodlar/cascade.sql
+++ sql-kodlar/cascade.sql
@@ -11,7 +11,6 @@ ALTER TABLE personel DROP COLUMN sube_id;
 alter table personel add adres_id int references adres (adres_id) on delete cascade;
 alter table personel add sube_id int references sube(sube_id) on delete cascade;
 
-
 ALTER TABLE ürün_kategori DROP COLUMN ürün_id;
 alter table ürün_kategori add ürün_id int references ürün (ürün_id) on delete cascade;
 
