c5c04d9cae2b7bbc23bd1932794a8a30e9b20954
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-01-04 00:01:05 +0300

    Update function.sql

--- sql-kodlar/function.sql
+++ sql-kodlar/function.sql
@@ -1,1 +1,14 @@
+--Function işlemleri
 
+create function kdvHesapla(fiyat real)
+returns real
+language plpgsql
+as
+$$
+	begin 
+		fiyat:=fiyat*0.18+fiyat;
+		return fiyat;
+	end;
+$$
+
+select *,kdvHesapla(fiyat) as kdvli from menü;
