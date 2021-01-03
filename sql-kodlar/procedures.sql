6d7bc4f379d0e161221438281f36f2ce7e6e4470
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-01-03 23:46:36 +0300

    Update procedures.sql

--- sql-kodlar/procedures.sql
+++ sql-kodlar/procedures.sql
@@ -1,1 +1,12 @@
+--Procedure işlemleri
 
+--Ornek procedure işlemi
+create or replace  procedure pr_unvan(unvan varchar)
+language sql
+as
+$$
+	insert into unvan values(default,unvan);
+$$
+--procedure çagırma
+call pr_unvan('Motorlu Kurye');
+-----------------------------------------------------------------------
