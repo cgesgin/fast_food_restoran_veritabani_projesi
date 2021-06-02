9a024d17bc81355c16a63865b8da2bca9f5d0c31
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-06-03 00:57:57 +0300

    Update 09_sequences_restart.sql

--- sql-kodlar/09_sequences_restart.sql
+++ sql-kodlar/09_sequences_restart.sql
@@ -8,3 +8,9 @@ ALTER SEQUENCE kategori_kategori_id_seq RESTART WITH 13;
 insert into kategori (kategori_ad) values('veri');
 
 select * from kategori;
+
+--tablodaki en son kaydın id'ni döndöüren kod
+INSERT INTO category (name) VALUES ('John') RETURNING category_id
+
+--max id
+select max(tablo_id) from tablo_ad;
